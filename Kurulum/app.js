60f22a1d5777ff51abd4bba3ce830a6699b1b43d
kubraacelik <kkbra.celik92@gmail.com>
2024-02-22 20:28:01 +0300

    added js

---
+++ Kurulum/app.js
@@ -0,0 +1,42 @@
+//!DATA TYPES
+// number --> let age:number=22;
+// string --> let firstName:string="Kübra"
+// boolean --> let isPresent:boolean=true;
+// any --> let someThing : any = "Hello"
+// array --> let firstNames:string[] = ["Can","Elif","Ayşe"];
+// array --> let firstNames:Array<string> = ["Can","Elif","Ayşe"];
+// tuple --> let instructor:[number,string] = [1, "Can"];
+// tuple --> let user:[number,string,boolean,number,string] = [1,"Steve",true,20,"Admin"];
+//object için interface'te kullanıldı
+// type Person = {
+//     firstName : string,
+//     lastName : string, 
+//     age : number,
+//     jobTitle : string
+// }
+// let instructor : Person;
+// instructor = {
+//     firstName : "Can",
+//     lastName : "Boz",
+//     age : 29,
+//     jobTitle : "Software Instructor"
+// }
+// enum Media {
+//     Newspaper,
+//     Newsletter,
+//     Magazine,
+//     Book
+// }
+// console.log(Media.Book);
+//geriye bir şey döndürmüyorsa(return) void data type'i verilir
+// function sayHello() : void{
+//     console.log("Hi");
+// }
+// sayHello();
+// union
+// let code : string | number = "123";
+// code = 235;
+// code = "Ali"
+// let employeeName:string="John Smith";
+// let employeeDept:string="Finance"
+// let employeeDesc:string=`${employeeName} works in the ${employeeDept} department`;
