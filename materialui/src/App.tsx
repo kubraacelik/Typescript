8ce10048d97770b5b889fe2c879406297fe520d8
kubraacelik <kkbra.celik92@gmail.com>
2024-03-01 21:48:07 +0300

    updated

--- materialui/src/App.tsx
+++ materialui/src/App.tsx
@@ -4,13 +4,15 @@ import './App.css';
 import LessonTypography from './components/LessonTypography';
 import LessonButton from './components/LessonButton';
 import LessonButtonGroup from './components/LessonButtonGroup';
+import LessonTextField from './components/LessonTextField';
 
 function App() {
   return (
     <div className="App">
       {/* <LessonTypography/> */}
       {/* <LessonButton/> */}
-      <LessonButtonGroup/>
+      {/* <LessonButtonGroup/> */}
+      <LessonTextField/>
     </div>
   );
 }
