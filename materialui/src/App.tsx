b6f2e5fbd9dc6d73080eb0c3a011d7a6e6173bc4
kubraacelik <kkbra.celik92@gmail.com>
2024-03-01 16:08:05 +0300

    first commit

---
+++ materialui/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LessonTypography from './components/LessonTypography';
+import LessonButton from './components/LessonButton';
+import LessonButtonGroup from './components/LessonButtonGroup';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LessonTypography/> */}
+      {/* <LessonButton/> */}
+      <LessonButtonGroup/>
+    </div>
+  );
+}
+
+export default App;
