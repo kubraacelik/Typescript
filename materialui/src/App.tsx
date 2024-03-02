e01d39db38219a19c0c7e4030a815725dc96c2e4
kubraacelik <kkbra.celik92@gmail.com>
2024-03-02 15:07:34 +0300

    commit

--- materialui/src/App.tsx
+++ materialui/src/App.tsx
@@ -5,6 +5,9 @@ import LessonTypography from './components/LessonTypography';
 import LessonButton from './components/LessonButton';
 import LessonButtonGroup from './components/LessonButtonGroup';
 import LessonTextField from './components/LessonTextField';
+import LessonRadioGroup from './components/LessonRadioGroup';
+import { LessonRadioExample } from './components/LessonRadioExample';
+import { LessonSelect } from './components/LessonSelect';
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
       {/* <LessonTypography/> */}
       {/* <LessonButton/> */}
       {/* <LessonButtonGroup/> */}
-      <LessonTextField/>
+      {/* <LessonTextField/> */}
+      {/* <LessonRadioGroup/> */}
+      {/* <LessonRadioExample /> */}
+      <LessonSelect />
     </div>
   );
 }
