c9375e8f7184cd280ead80a3b6b4e8d5d837a023
kubraacelik <kkbra.celik92@gmail.com>
2024-03-02 17:33:03 +0300

    learned Select ui

--- materialui/src/components/LessonSelect.tsx
+++ materialui/src/components/LessonSelect.tsx
@@ -1,14 +1,27 @@
 import { Box, MenuItem, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const LessonSelect = () => {
+
+  const [value, setValue] = useState<string[]>([])
+
+  console.log(value);
+
   return (
-      <Box width="250px">
-          <TextField label="Ülke Seçiniz" select fullWidth>
-              <MenuItem value="TR">Türkiye</MenuItem>
-              <MenuItem value="USA">ABD</MenuItem>
-              <MenuItem value="FR">France</MenuItem>
-        </TextField>
-      </Box>
+    <Box width="250px">
+      {/* <TextField label="Ülke Seçiniz" select fullWidth value={value} onChange={(e) => setValue(e.target.value)}>
+        <MenuItem value="TR">Türkiye</MenuItem>
+        <MenuItem value="USA">ABD</MenuItem>
+        <MenuItem value="FR">France</MenuItem>
+      </TextField> */}
+      <TextField SelectProps={{
+        multiple: true,
+      }} label="Ülke Seçiniz" select fullWidth value={value} onChange={(e) => setValue(typeof e.target.value === 'string' ? e.target.value.split(',') : e.target.value)}>
+        <MenuItem value="TR">Türkiye</MenuItem>
+        <MenuItem value="USA">ABD</MenuItem>
+        <MenuItem value="FR">France</MenuItem>
+      </TextField>
+
+    </Box>
   )
 }
