fb55e3d9f68cb3a7c9ab9359b73675175fac782b
kubraacelik <kkbra.celik92@gmail.com>
2024-03-03 16:01:57 +0300

    learned ImageList ui

---
+++ materialui/src/components/LessonImageList.tsx
@@ -0,0 +1,107 @@
+import { ImageList, ImageListItem, ImageListItemBar, Stack } from '@mui/material'
+import React from 'react'
+
+export const LessonImageList = () => {
+    return (
+        <Stack>
+            {/* rowHeight: 1 resmin yüksekliği kaç px olsun.
+            cols : 1 satıra kaç resim sığsın
+            sx={{ width: 500, height: 450 }} : Resim listesinin büyüklüğü ve genişliği */}
+            <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img src={item.img} alt="" loading='lazy' />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+            <p>--------------------------------------------------------------------------------------------------------------</p>
+            <ImageList cols={3}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img src={item.img} alt="" loading='lazy' />
+                        <ImageListItemBar title={item.title} />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+            <p>--------------------------------------------------------------------------------------------------------------</p>
+            <ImageList sx={{ width: 500, height: 450 }}
+                variant="quilted"
+                cols={4}
+                rowHeight={121}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img src={item.img} alt="" loading='lazy' />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+            <p>--------------------------------------------------------------------------------------------------------------</p>
+            <ImageList sx={{ width: 500, height: 450 }} variant="woven" cols={3} gap={8}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img src={item.img} alt="" loading='lazy' />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+            <p>--------------------------------------------------------------------------------------------------------------</p>
+            <ImageList variant="masonry" cols={3} gap={8}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img src={item.img} alt="" loading='lazy' />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </Stack>
+    )
+}
+
+const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        title: 'Honey',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        title: 'Basketball',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        title: 'Fern',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+        title: 'Mushrooms',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+        title: 'Tomato basil',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+        title: 'Sea star',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+        title: 'Bike',
+    },
+];
+
