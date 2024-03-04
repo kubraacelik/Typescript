efb3b9508da00f0ed5b0202fb853efd68bb65c53
kubraacelik <kkbra.celik92@gmail.com>
2024-03-04 21:23:04 +0300

    updated

--- materialui/src/App.tsx
+++ materialui/src/App.tsx
@@ -72,7 +72,7 @@ function App() {
       {/* <LessonSkeleton /> */}
       {/* <LessonLoadingButton /> */}
       {/* <LessonTable /> */}
-      {/* <LessonDateTime /> */}
+      <LessonDateTime />
     </div>
   );
 }
