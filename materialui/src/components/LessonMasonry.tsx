e19db12812fa67db4a1504fc87ed7f27589d8c9e
kubraacelik <kkbra.celik92@gmail.com>
2024-03-05 00:01:30 +0300

    learned Masonry ui

---
+++ materialui/src/components/LessonMasonry.tsx
@@ -0,0 +1,25 @@
+import { Masonry } from '@mui/lab'
+import { Accordion, AccordionDetails, AccordionSummary, Paper } from '@mui/material'
+import React from 'react'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const heights = [150, 30, 90, 70, 90, 100, 150, 30, 50, 80]
+
+export const LessonMasonry = () => {
+    return (
+        <Masonry columns={3} spacing={2}>
+            {heights.map((height, index) => (
+                <Paper key={index}>
+                    <Accordion sx={{ minHeight: height }}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            Accordion {index + 1}
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Contents
+                        </AccordionDetails>
+                    </Accordion>
+                </Paper>
+            ))}
+        </Masonry>
+    )
+}
