a681d557c6128bb0c407cf061cc1d4531e2de12f
kubraacelik <kkbra.celik92@gmail.com>
2024-03-04 21:23:10 +0300

    commmit

--- materialui/src/components/LessonDateTime.tsx
+++ materialui/src/components/LessonDateTime.tsx
@@ -1,32 +1,27 @@
-import { DatePicker } from '@mui/lab';
-import { Stack, TextField } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { useState } from 'react';
+import { Stack, TextField } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import React from 'react'
+import { useState } from 'react'
 import dayjs, { Dayjs } from 'dayjs';
 
 export const LessonDateTime = () => {
     const [value, setValue] = useState<Dayjs | null>(null)
+    console.log(value);
+    
     return (
-        // renderInput : içerisindeki text'ten değeri alıp okuma işlemini yapar
+        // renderInput : içerisindeki text'ten değeri alıp okuma işlemi yapar
         <Stack>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker
                     value={value}
-                    onChange={(newValue: any) => {
+                    onChange={(newValue) => {
                         setValue(newValue)
                     }}
                     label='Tarih'
                     renderInput={(params: any) => <TextField {...params} />}
                 />
             </LocalizationProvider>
-
-            <DatePicker label="Uncontrolled picker" defaultValue={dayjs('2022-04-17')} />
-            <DatePicker
-                label="Controlled picker"
-                value={value}
-                onChange={(newValue:any) => setValue(newValue)}
-            />
         </Stack>
-    );
+    )
 }
