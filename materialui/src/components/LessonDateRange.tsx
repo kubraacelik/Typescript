fada389ab09c9f19c48dcfa80da672710929a729
kubraacelik <kkbra.celik92@gmail.com>
2024-03-04 22:09:50 +0300

    commit

--- materialui/src/components/LessonDateRange.tsx
+++ materialui/src/components/LessonDateRange.tsx
@@ -1,17 +1,17 @@
 import { Stack, TextField } from '@mui/material'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
-import {DateRangePicker, DateRange} from '@mui/x-date-pickers-pro/DateRangePicker'
+import {DateRangePicker, DateRange} from '@mui/x-date-pickers-pro/DateRangePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import React from 'react'
 import { useState } from 'react'
 import dayjs, { Dayjs } from 'dayjs';
 
 export const LessonDateRange = () => {
-  const [value, setValue] = useState<Dayjs | null>(null)
+  const [value, setValue] = useState<DateRange<>>([null,null])
   return (
     <Stack>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <TimePicker 
+                <DateRangePicker 
                 label='Time'
                 value={value}
                 onChange={(newValue)=>{
