45b68d3028026c8ea176c004b8b0b1a421b9db10
kubraacelik <kkbra.celik92@gmail.com>
2024-03-04 23:31:08 +0300

    learned DateRange ui

--- materialui/src/components/LessonDateRange.tsx
+++ materialui/src/components/LessonDateRange.tsx
@@ -1,24 +1,35 @@
-import { Stack, TextField } from '@mui/material'
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
-import {DateRangePicker, DateRange} from '@mui/x-date-pickers-pro/DateRangePicker';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
+import Stack from '@mui/material/Stack';
+import { Box } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import {
+  DateRangePicker,
+  DateRange,
+} from '@mui/x-date-pickers-pro/DateRangePicker';
+import { DatePicker } from '@mui/x-date-pickers';
+// import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+// import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { useState } from 'react';
 
 export const LessonDateRange = () => {
-  const [value, setValue] = useState<DateRange<>>([null,null])
+  const [value, setValue] = useState<DateRange<Dayjs>>([null, null]);
   return (
     <Stack>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateRangePicker 
-                label='Time'
-                value={value}
-                onChange={(newValue)=>{
-                    setValue(newValue)
-                }}
-                />
-            </LocalizationProvider>
-        </Stack>
-  )
-}
+      <LocalizationProvider
+        dateAdapter={AdapterDayjs}
+        localeText={{ start: 'Baslangıc', end: 'Bitiş' }}
+      >
+        <DateRangePicker
+          label="Time"
+          value={value}
+          onChange={(newValue:any) => {
+            setValue(newValue);
+          }}
+        />
+      </LocalizationProvider>
+    </Stack>
+  );
+};
